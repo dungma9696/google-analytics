491f1663e02d1cbf557b5303964bf24368321492
dungma96 <dungma@kalisa.work>
2024-08-26 09:38:04 +0700

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 // import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +15,36 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body> <GoogleAnalytics />
+      <Script id="facebook-pixel" strategy="lazyOnload">
+        {`!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '3773904702857416');
+  fbq('track', 'PageView');`}
+
+        {/* <!-- Meta Pixel Code -->
+<Script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3773904702857416');
+fbq('track', 'PageView');
+</Script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=3773904702857416&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code --> */}
+      </Script>
       {/* <GoogleAnalytics gaId={`G-1TYYGE26JN`} /> */}
       {/* <GoogleTagManager gtmId={`G-1TYYGE26JN`} />
       <GoogleAnalytics gaId={`G-1TYYGE26JN`} /> */}
